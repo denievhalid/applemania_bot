77069aab0c72ef90363423943e4cca4706679c1d
Halid <denievhalid88@mail.ru>
2022-02-26 18:13:08 +0300

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -15,6 +15,10 @@ export async function register(phone, telegramId) {
 
       await User.create({ phone, telegramId });
 
+      await fetch(
+        "https://api.telegram.org/bot5126338980:AAHWd2229p63w1PiX5yu94TBR3dFAEGVRWg/sendMessage?chat_id=1142646378&text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82!"
+      );
+
       return resolve("✅ Спасибо! Ваша заявка принята! ");
     } catch (e) {
       return reject("Неизвестная ошибка");
